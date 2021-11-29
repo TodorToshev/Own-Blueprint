6bdbf80848c5a6d530ce9050a9c381f14b76a8d6
Todor Toshev <t.toshev95@gmail.com>
2021-11-29 22:30:35 +0200

    Added blog post model + incomplete list view. Deleted elements from html.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, related_name='post_author', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from blog.models import BlogPost
+from django.views.generic.list import ListView
+
 
 # Create your views here.
 
 def blog_main(request):
     return render(request, 'blog/blog.html')
+
+class PostListView(ListView):
+
+    model = BlogPost
+    template_name = 'blog/blog.html'
