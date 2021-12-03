a865050cdb989ea0d06e603fdc6629cf33ddd6ff
Todor Toshev <t.toshev95@gmail.com>
2021-12-04 00:43:20 +0200

    Changed main urls to point to accounts.urls for login/logout; Added register url + extended UserCreationForm to save email + user_registration FBV using the custom form; Changed html accordingly.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
+
+def user_registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful.')
+            return redirect("blog:blog")
+        messages.error(request, 'Registration failed.')
+    else:
+        form = UserRegistrationForm()
+    return render(request, template_name='registration/registration.html', context={'form': form})
