e33716819e5880190d39898bade767f153dab745
Todor Toshev <t.toshev95@gmail.com>
2021-12-16 21:30:32 +0200

    Startapp shop; slight changes to blog html.

--- blog/admin.py
+++ blog/admin.py
@@ -3,14 +3,12 @@ from .models import BlogPost, Category, PostComment
 
 # Register your models here.
 
-# admin.site.register(BlogPost)
-# admin.site.register(PostComment)
-
 admin.site.register(Category)
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'tags', 'created', 'updated', 'views']
+    list_display = ['title', 'author', 'created', 'updated', 'views']
+
 
 @admin.register(PostComment)
 class PostCommentAdmin(admin.ModelAdmin):
