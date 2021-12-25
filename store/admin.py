4051717357c1bf8f8eda70df75413f18a57a4418
Todor Toshev <t.toshev95@gmail.com>
2021-12-25 21:51:38 +0200

    Fixed 'sizes' field; added new Image model class and new ModelAdmin class to allow adding images in the Product admin page.

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import Product, Size
+from .models import Product, Size, ProductImage
 
 # Register your models here.
 
-admin.site.register(Product)
-
 admin.site.register(Size)
+
+
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+ 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    '''
+    create a single Django admin from two different models. 
+    With inlines, the admin can allow editing of parent and child models on one page.
+    '''
+    inlines = [ProductImageAdmin]
+ 
+    #probably not needed:
+    # class Meta:
+    #    model = Product
--- store/models.py
+++ store/models.py
@@ -6,10 +6,6 @@ from decimal import Decimal
 # Create your models here.
 
 
-class Image(models.Model):
-    image = models.ImageField(upload_to='products/%Y/%m/%d')
-
-
 class Size(models.Model):
     size = models.CharField(max_length=5)
 
@@ -21,15 +17,16 @@ class Product(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
-    images = models.ForeignKey(
-        Image, related_name='product_image', on_delete=models.SET_NULL, null=True, blank=True)
     rating = models.PositiveIntegerField(
         null=True, blank=True, validators=[MaxValueValidator(5)])
     date_added = models.DateTimeField(null=True, blank=True, auto_now_add=True)
     tags = TaggableManager()
-    #TODO: many to many
-    sizes = models.ForeignKey(Size, related_name='product_size',
-                              on_delete=models.SET_NULL, null=True, blank=True)
+    sizes = models.ManyToManyField(Size, related_name='product_size')
 
     def __str__(self):
         return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, null=True)
