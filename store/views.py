a65259ba485ddad26c6d3b726df34a12c29f90b6
Todor Toshev <t.toshev95@gmail.com>
2021-12-28 22:29:58 +0200

    Added SizeForm, which accepts product instance from the view and uses its sizes as field choices.

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Product
+from .forms import SizeForm
 
 # Create your views here.
 
@@ -13,5 +14,8 @@ def products(request):
 def single_product(request, pk):
     product = get_object_or_404(Product, pk=pk)
     product_sizes = product.sizes.all()
-    context = {'product': product, 'sizes': product_sizes}
+    
+    size_form = SizeForm(product)
+
+    context = {'product': product, 'sizes': product_sizes, 'size_form': size_form}
     return render(request, 'store/single-product.html', context)
