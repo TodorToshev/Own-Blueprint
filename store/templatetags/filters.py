d35163318b68f811cdc519a98b5ea3137f4de962
Todor Toshev <t.toshev95@gmail.com>
2022-01-06 16:34:37 +0200

    Custom template filter now uses 'round()' instead of int(), that way the 'for' loop displays 4 stars instead of 3 when rating is 3.7 for eg. Minor HTML+CSS changes.

--- store/templatetags/filters.py
+++ store/templatetags/filters.py
@@ -5,9 +5,9 @@ register = template.Library()
 #returns range in order to work w/ 'for' loop for star rating
 @register.filter(name='times') 
 def times(number):
-    return range(int(number))
+    return range(round(number))
 
 #returns the rest of that range for 'fa-star-o empty' class
 @register.filter(name='rest')
 def rest(number):
-    return range(5 - int(number))
+    return range(5 - round(number))
