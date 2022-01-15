30b712ed11a353a6705e11e46718e35c484db2a6
Todor Toshev <t.toshev95@gmail.com>
2022-01-15 10:56:38 +0200

    Fixed cart objects getting deleted because of session change on logout.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('login/', views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path("register/", views.user_registration, name="registration"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,59 @@ from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 from django.contrib.auth import login
 from django.contrib import messages
+from django.contrib.auth import views as auth_views
+from django.utils.decorators import method_decorator
+from functools import wraps
+
 
 # Create your views here.
+
+
+class persist_session_vars(object):
+    """
+    Some views, such as login and logout, will reset all session state.
+    (via a call to ``request.session.cycle_key()`` or ``session.flush()``).
+    That is a security measure to mitigate session fixation vulnerabilities.
+
+    By applying this decorator, some values are retained.
+    Be very aware what kind of variables you want to persist.
+    """
+
+    def __init__(self, vars):
+        self.vars = vars
+
+    def __call__(self, view_func):
+
+        @wraps(view_func)
+        def inner(request, *args, **kwargs):
+            session_backup = {}
+            for var in self.vars:
+                try:
+                    session_backup[var] = request.session[var]
+                except KeyError:
+                    pass
+
+            # Call the original view
+            response = view_func(request, *args, **kwargs)
+
+            # Restore variables in the new session
+            for var, value in session_backup.items():
+                request.session[var] = value
+
+            return response
+
+        return inner
+
+
+#no need to override this view, 'cart' doesn't get deleted on login or register.
+# @method_decorator(persist_session_vars(['cart']), name='dispatch')
+# class LoginView(auth_views.LoginView):
+#     pass
+
+@method_decorator(persist_session_vars(['cart']), name='dispatch')
+class LogoutView(auth_views.LogoutView):
+    pass
+
 
 def user_registration(request):
     if request.method == 'POST':
