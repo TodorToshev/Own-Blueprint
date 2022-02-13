ef0534dbb6f61479de0099a9bceb518afd2eb158
Todor Toshev <t.toshev95@gmail.com>
2022-02-13 11:19:48 +0200

    Added slashes to urlpatterns, removed 'lorem' tag from html.

--- api/urls.py
+++ api/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('posts/', views.PostListView.as_view()),
     path('comments/', views.CommentListView.as_view()),
     path('comment-create/', views.CommentCreateView.as_view()),
-    path('comment-update/<int:pk>', views.CommentUpdateDeleteView.as_view()),
+    path('comment-update/<int:pk>/', views.CommentUpdateDeleteView.as_view()),
     path('post-create/', views.PostCreateView.as_view()),
-    path('post-update/<int:pk>', views.PostUpdateDeleteView.as_view(), name='manufacturers'),
+    path('post-update/<int:pk>/', views.PostUpdateDeleteView.as_view(), name='manufacturers'),
     path('register/', views.UserCreateView.as_view()),
     
 
